5f7310af2f84c6a7ba1ea94bd744ee939f2bcd71
Ironowl1907 <facuguialtana7@gmail.com>
2026-03-13 23:22:39 +0000

    New endpoint, fetching all stops

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
 # Mendotran requester
 import stops as stops
 
-# HACK: lat, lon, latSpan and lonSpan should change dynamically
-mendotran_request_stops_params = {
-    "platform": "web",
-    "v": "",
-    "lat": "",
-    "lon": "",
-    "latSpan": "",
-    "lonSpan": "",
-    "version": "1.0",
-}
-
-
-def make_stop_request_params(lat: float, lon: float, latSpan: float, lonSpan: float):
-    params = dict(mendotran_request_stops_params)
-    params["lat"] = str(lat)
-    params["lon"] = str(lon)
-    params["latSpan"] = str(latSpan)
-    params["lonSpan"] = str(lonSpan)
-    return params
-
 
 def main():
-    r = stops.mendotran_request_stops(
-        make_stop_request_params(-32.74, -68.70, 0.37, 1.28))
-    print(r.content)
+    r = stops.mendotran_request_stops()
+    if (r.status_code == 200):
+        print(r.text)
+    else:
+        print(f"Failed with exit code: {r.status_code}")
 
 
 if __name__ == "__main__":
--- stops.py
+++ stops.py
@@ -1,19 +1,28 @@
 import requests
 
-stops_url = "https://mendotran.oba.visionblo.com/oba_api/api/where/stops-for-location.json"
-
+stops_url = "https://owa.visionblo.com/api/mendoza/search"
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
     "Accept": "*/*",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive",
-    "Referer": "https://mendotran.oba.visionblo.com/",
+    "Accept-Language": "es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Content-Type": "application/json",
+    "Origin": "https://owa.visionblo.com",
+    "Referer": "https://owa.visionblo.com/web/mendoza/",
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
 }
 
 
-def mendotran_request_stops(params):
-    r = requests.get(stops_url, params=params, headers=headers)
+def mendotran_request_stops():
+    payload = {
+        "token": "OQkGfHEQqWRO9zXRQgJb",
+        "text": "",
+        "xss": "86adb365fced6934d3ff6bec",
+        "search": ["stops"],
+        "no_favorites": True
+    }
+    r = requests.post(stops_url, json=payload, headers=headers)
     return r
