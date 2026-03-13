7350ae5cf0bef5026c307ed24c11f35e78be67ba
Ironowl1907 <facuguialtana7@gmail.com>
2026-03-13 17:17:53 +0000

    Adding headers

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ headers = {
     "Origin": "https://owa.visionblo.com"
 }
 
+# HACK: lat, lon, latSpan and lonSpan should change dynamically
 mendotran_request_stops_header = {
+    "platform":"web",
+    "v": "",
+    "lat":"-32.09676406635843",
+    "lon":"-69.62585449218751",
+    "latSpan":"4.8667193939577835",
+    "lonSpan":"9.788818359375",
+    "version":"1.0",
 }
 
 
 def main() :
-    mendotran_request_stops({})
+    mendotran_request_stops(mendotran_request_stops_header)
 
 def mendotran_request_stops(payload):
     r = requests.get(url, data = payload, headers = headers)
