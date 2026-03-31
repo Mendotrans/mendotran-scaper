0fd2679246594f23fbe2d93ae8f6da8946894365
Ironowl1907 <facuguialtana7@gmail.com>
2026-03-31 14:06:11 +0000

    Checking for database existance before recreating

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # Mendotran requester
 import stops as stops
 import json
+import os
 
 
 def main():
+    if os.path.isfile("mendotran.db"):
+        print("Found DB!")
+        exit(0)
+
+    print("No DB found!")
     json_services_data = stops.mendotran_request_services()
     json_stops_data = stops.mendotran_request_stops()
     r = stops.mendotran_generate_db(json_stops_data, json_services_data)
     r_formated = json.dumps(r, indent=4)
     print(r_formated)
 
-    pass
+
+def somethingelese():
+    r = stops.mendotran_request_stops()
+    r_formated = json.dumps(r, indent=4)
+    print(r_formated)
 
 
 if __name__ == "__main__":
